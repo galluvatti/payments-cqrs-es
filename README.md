504a863449fcf6e3bbed71820872feffe45b5b81
Giovanni Alluvatti <giovanni.alluvatti@gmail.com>
2024-12-29 08:33:16 +0100

    Initial commit: Gradle build file, logback conf, docker-compose

---
+++ README.md
@@ -0,0 +1,8 @@
+# Technologies
+
+## Command API
+- JDK 17
+- Spring Boot 3.4.1
+- Spring Kafka
+- Spring Web
+- Spring Mongo
---
+++ paymentgateway/src/main/kotlin/com/mypay/paymentgateway/PaymentgatewayApplication.kt
@@ -0,0 +1,11 @@
+package com.mypay.paymentgateway
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class PaymentgatewayApplication
+
+fun main(args: Array<String>) {
+	runApplication<PaymentgatewayApplication>(*args)
+}
