4fa4b39333fa65b5e8df4df04a6e15b15cafd395
Giovanni Alluvatti <giovanni.alluvatti@gmail.com>
2025-01-03 11:53:43 +0100

    skeleton project for the Query API

---
+++ payments-query/src/main/kotlin/com/mypay/payments_query/PaymentsQueryApplication.kt
@@ -0,0 +1,11 @@
+package com.mypay.payments_query
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class PaymentsQueryApplication
+
+fun main(args: Array<String>) {
+	runApplication<PaymentsQueryApplication>(*args)
+}
